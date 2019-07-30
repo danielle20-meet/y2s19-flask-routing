0d465da6f49358be273b0d32e7723e1d2095ea3f
myname21-meet <myname21@meet.mit.edu>
2019-07-30 10:25:21 +0300

    routing

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "home page" 
+def home(Name="Danielle"):
+    return render_template("home.html",Name=Name)
+@app.route('/student/<int:student_id>')
+def display_student(student_id):
+ 	return render_template("student.html",student_id=student_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
